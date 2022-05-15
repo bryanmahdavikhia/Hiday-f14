ca0a6dca139efa16efb77f38102412c3d814da88
Mohammad Bryan Mahdavikhia <mohammad.bryan@ui.ac.id>
2022-05-15 17:10:04 +0700

    try to fix

--- aset/models.py
+++ aset/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class dropDownHewan(models.Model):
-    jenisHewan = models.CharField(max_length=100)
+# class dropDownHewan(models.Model):
+#     jenisHewan = models.CharField(max_length=100)
 
-class dropDownTanaman(models.Model):
-    jenisTanaman = models.CharField(max_length=100)
+# class dropDownTanaman(models.Model):
+#     jenisTanaman = models.CharField(max_length=100)
