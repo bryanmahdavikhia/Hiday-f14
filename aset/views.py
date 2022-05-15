f804b1ecfa5e989d349c7493206579adab6544d6
Dyta Dewipuspita <dyta.dewipuspita01@ui.ac.id>
2022-05-15 19:07:11 +0700

    added update dan buat transaksi beli aset

--- aset/views.py
+++ aset/views.py
@@ -84,60 +84,91 @@ def list_dekorasi(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
 
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
+
     cursor.execute("SET search_path TO hidayf14")
     query = "SELECT id, nama, minimum_level, harga_beli, harga_jual FROM aset, dekorasi WHERE id=id_aset;"
     cursor.execute(query)
     data = cursor.fetchall()
-    return render(request, 'list_dekorasi.html', {'data': data})
+    return render(request, 'list_dekorasi.html', {'data': data, 'role': role})
 
 def list_bibit_tanaman(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
 
     cursor.execute("SET search_path TO hidayf14")
     query = "SELECT id, nama, minimum_level, harga_beli, durasi_panen FROM aset, bibit_tanaman WHERE id=id_aset;"
     cursor.execute(query)
     data = cursor.fetchall()
-    return render(request, 'list_bibit_tanaman.html', {'data': data})
+    return render(request, 'list_bibit_tanaman.html', {'data': data,'role': role})
 
 def list_kandang(request):
     cursor = connection.cursor()
     
     cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
 
     cursor.execute("SET search_path TO hidayf14")
     query = "SELECT id, nama, minimum_level, harga_beli, kapasitas_maks, jenis_hewan FROM aset, kandang WHERE id=id_aset;"
     cursor.execute(query)
     data = cursor.fetchall()
-    return render(request, 'list_kandang.html', {'data': data})
+    return render(request, 'list_kandang.html', {'data': data,'role': role})
 
 def list_hewan(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
 
     cursor.execute("SET search_path TO hidayf14")
     query = "SELECT a.id, a.nama, a.minimum_level, a.harga_beli, h.durasi_produksi, h.id_kandang FROM aset AS a, hewan AS h, kandang as k WHERE A.id=H.id_aset AND k.id_aset = h.id_kandang;"
     cursor.execute(query)
     data = cursor.fetchall()
-    return render(request, 'list_hewan.html', {'data': data})
+    return render(request, 'list_hewan.html', {'data': data,'role': role})
 
 def list_alat_produksi(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
 
     cursor.execute("SET search_path TO hidayf14")
     query = "SELECT id, nama, minimum_level, harga_beli, kapasitas_maks FROM aset, alat_produksi WHERE id=id_aset;"
     cursor.execute(query)
     data = cursor.fetchall()
-    return render(request, 'list_alat_produksi.html', {'data': data})
+    return render(request, 'list_alat_produksi.html', {'data': data,'role': role})
 
 def list_petak_sawah(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
 
     cursor.execute("SET search_path TO hidayf14")
     query = "SELECT id, nama, minimum_level, harga_beli, jenis_tanaman FROM aset, petak_sawah WHERE id=id_aset;"
     cursor.execute(query)
     data = cursor.fetchall()
-    return render(request, 'list_petak_sawah.html', {'data': data})
+    return render(request, 'list_petak_sawah.html', {'data': data,'role': role})
     
