7eff450f2dc98f90bc2575fdc00101b1109e2b43
Dyta Dewipuspita <dyta.dewipuspita01@ui.ac.id>
2022-05-25 20:57:11 +0700

    improve asset

--- aset/urls.py
+++ aset/urls.py
@@ -9,8 +9,17 @@ urlpatterns = [
     path('', views.index, name='pilihan-buat-aset'),
     path('list-aset/', views.list_aset, name='list-aset'),
     path('beli-aset/', views.form_beli_aset, name='beli-aset'),
+
     path('list-transaksi-beli-aset/', views.list_transaksi_pembelian_aset, name='list-transaksi-beli-aset'),
-    path('list-koleksi-aset/', views.list_koleksi_aset, name='list-koleksi-aset'),
+    path('pilihan-koleksi-aset/', views.pilihan_list_koleksi_aset, name='pilihan-koleksi-aset'),
+
+    path('pilihan-koleksi-aset/list-koleksi-dekorasi/', views.list_koleksi_dekorasi, name='list-koleksi-dekorasi'),
+    path('pilihan-koleksi-aset/list-koleksi-bibit/', views.list_koleksi_bibit, name='list-koleksi-bibit'),
+    path('pilihan-koleksi-aset/list-koleksi-kandang/', views.list_koleksi_kandang, name='list-koleksi-kandang'),
+    path('pilihan-koleksi-aset/list-koleksi-hewan/', views.list_koleksi_hewan, name='list-koleksi-hewan'),
+    path('pilihan-koleksi-aset/list-koleksi-alat/', views.list_koleksi_alat_produksi, name='list-koleksi-alat'),
+    path('pilihan-koleksi-aset/list-koleksi-petak-sawah/', views.list_koleksi_petak_sawah, name='list-koleksi-sawah'),
+
     path('form-bibit/', views.form_bibit_tanaman, name='form-bibit'),
     path('form-dekorasi/', views.form_dekorasi, name='form-dekorasi'),
     path('form-kandang/', views.form_kandang, name='form-kandang'),
@@ -19,21 +28,21 @@ urlpatterns = [
     path('form-petak-sawah/', views.form_petak_sawah, name='form-petak-sawah'),
 
     path('list-aset/list-dekorasi/', views.list_dekorasi, name='list-dekorasi'),
-    path('list-aset/list-dekorasi/update-dekorasi/', views.update_dekorasi, name='update-dekorasi'),
+    path('list-aset/list-dekorasi/update-dekorasi/<str:nama>/', views.update_dekorasi, name='update-dekorasi'),
 
     path('list-aset/list-bibit-tanaman/', views.list_bibit_tanaman, name='list-bibit-tanaman'),
-    path('list-aset/list-bibit-tanaman/update-bibit/', views.update_bibit, name='update-bibit'),
+    path('list-aset/list-bibit-tanaman/update-bibit/<str:nama>/', views.update_bibit, name='update-bibit'),
 
     path('list-aset/list-kandang/', views.list_kandang, name='list-kandang'),
-    path('list-aset/list-kandang/update-kandang/', views.update_kandang, name='update-kandang'),
+    path('list-aset/list-kandang/update-kandang/<str:nama>/', views.update_kandang, name='update-kandang'),
 
     path('list-aset/list-hewan/', views.list_hewan, name='list-hewan'),
-    path('list-aset/list-hewan/update-hewan/', views.update_hewan, name='update-hewan'),
+    path('list-aset/list-hewan/update-hewan/<str:nama>/', views.update_hewan, name='update-hewan'),
 
     path('list-aset/list-alat-produksi/', views.list_alat_produksi, name='list-alat'),
-    path('list-aset/list-alat-produksi/update-alat/', views.update_alat, name='update-alat'),
+    path('list-aset/list-alat-produksi/update-alat/<str:nama>/', views.update_alat, name='update-alat'),
 
     path('list-aset/list-petak-sawah/', views.list_petak_sawah, name='list-petak-sawah'),
-    path('list-aset/list-petak-sawah/update-sawah/', views.update_petak_sawah, name='update-petak-sawah')
+    path('list-aset/list-petak-sawah/update-sawah/<str:nama>/', views.update_petak_sawah, name='update-sawah')
     
 ]
--- aset/views.py
+++ aset/views.py
@@ -7,42 +7,116 @@ from django.shortcuts import redirect, render
 def index(request):
     return render(request, 'createAsset.html')
 
+def pilihan_list_koleksi_aset(request):
+    return render(request, 'list_pilihan_koleksi_aset.html')
+
 def form_bibit_tanaman(request):
-    return render(request, 'form_bibit_tanaman.html')
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        role = request.session['role']
+        if role == "admin":
+            if request.method == "POST":
+                nama = request.POST["nama"]
+                min_level = request.POST["min_level"]
+                harga_beli = request.POST["harga_beli"]
+                durasi_panen = request.POST["durasi_panen"]
+            else:
+                return render(request, 'form_bibit_tanaman.html')
 
 def form_dekorasi(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
-    return render(request, 'form_dekorasi.html')
+    if request.session.has_key('account'):
+        role = request.session['role']
+        if role == "admin":
+            if request.method == "POST":
+                nama = request.POST["nama"]
+                min_level = request.POST["min_level"]
+                harga_beli = request.POST["harga_beli"]
+                harga_jual = request.POST["harga_jual"]
+            else:
+                return render(request, 'form_dekorasi.html')
 
 def form_kandang(request):
     cursor = connection.cursor()
-    cursor.execute("SET search_path TO hidayf14")
-    cursor.execute("SELECT nama FROM aset, bibit_tanaman WHERE id=id_aset;")
-    data = cursor.fetchall()
-    return render(request, 'form_kandang.html',{"data":data})
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        role = request.session['role']
+        if role == "admin":
+            if request.method == "POST":
+                nama = request.POST["nama"]
+                min_level = request.POST["min_level"]
+                harga_beli = request.POST["harga_beli"]
+                kapasitas_maks = request.POST["kapasitas_maks"]
+                jenis_hewan = request.POST["jenis_hewan"]
+            else:
+                cursor.execute("SET search_path TO hidayf14")
+                cursor.execute("SELECT nama FROM aset, bibit_tanaman WHERE id=id_aset;")
+                data = cursor.fetchall()
+                return render(request, 'form_kandang.html',{"data":data})
 
 def form_hewan(request):
-    return render(request, 'form_hewan.html')
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        role = request.session['role']
+        if role == "admin":
+            if request.method == "POST":
+                nama = request.POST["nama"]
+                min_level = request.POST["min_level"]
+                harga_beli = request.POST["harga_beli"]
+                durasi_produksi = request.POST["durasi_produksi"]
+            else:
+                return render(request, 'form_hewan.html')
 
 def form_alat_produksi(request):
-    return render(request, 'form_alat_produksi.html')
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        role = request.session['role']
+        if role == "admin":
+            if request.method == "POST":
+                nama = request.POST["nama"]
+                min_level = request.POST["min_level"]
+                harga_beli = request.POST["harga_beli"]
+                kapasitas_maks = request.POST["kapasitas_maks"]
+            else:
+                return render(request, 'form_alat_produksi.html')
 
 def form_petak_sawah(request):
     cursor = connection.cursor()
-    cursor.execute("SET search_path TO hidayf14")
-    cursor.execute("SELECT nama FROM aset, bibit_tanaman WHERE id=id_aset;")
-    data = cursor.fetchall()
-    return render(request, 'form_petak_sawah.html',{"data":data})
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        role = request.session['role']
+        if role == "admin":
+            if request.method == "POST":
+                nama = request.POST["nama"]
+                min_level = request.POST["min_level"]
+                harga_beli = request.POST["harga_beli"]
+                kapasitas_maks = request.POST["kapasitas_maks"]
+                jenis_tanaman = request.POST["jenis_tanaman"]
+            else:
+                cursor.execute("SET search_path TO hidayf14")
+                cursor.execute("SELECT nama FROM aset, bibit_tanaman WHERE id=id_aset;")
+                data = cursor.fetchall()
+                return render(request, 'form_petak_sawah.html',{"data":data})
 
 def form_beli_aset(request):
     cursor = connection.cursor()
-    cursor.execute("SET search_path TO hidayf14") #harus dicek lagi
-
-    list_aset = "SELECT nama, harga_beli FROM aset;"
-    cursor.execute(list_aset)
-    data = cursor.fetchall()
-    return render(request, 'form_beli_aset.html',{"dropDownAset":data})
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        role = request.session['role']
+        if role == "admin":
+            if request.method == "POST":
+                detail_aset = request.POST["detail_aset"]
+                jumlah = request.POST["jumlah"]
+            else:
+                cursor.execute("SET search_path TO hidayf14") #harus dicek lagi
+                list_aset = "SELECT nama, harga_beli FROM aset;"
+                cursor.execute(list_aset)
+                data = cursor.fetchall()
+                return render(request, 'form_beli_aset.html',{"dropDownAset":data})
 
 def list_transaksi_pembelian_aset(request):
     cursor = connection.cursor()
@@ -62,27 +136,21 @@ def list_transaksi_pembelian_aset(request):
 def list_aset(request):
     return render(request, 'list_aset.html')
 
-def list_koleksi_aset(request):
-    
+def list_koleksi_dekorasi(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
 
     if request.session.has_key('account'):
-        role = request.session['role']
-        if role == "admin":
-            cursor.execute("SET search_path TO hidayf14") 
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
+    cursor.execute("SET search_path TO hidayf14") 
 
-            list_aset = "SELECT id_koleksi_aset, nama, minimum_level, harga_beli, jumlah FROM aset, koleksi_aset_memiliki_aset WHERE id=id_aset;"
-            cursor.execute(list_aset)
-            data = cursor.fetchall()
-            return render(request, 'list_koleksi_aset.html',{"data":data})
-        elif role == "pengguna":
-            cursor.execute("SET search_path TO hidayf14") 
-
-            list_aset = "SELECT nama, minimum_level, harga_beli, jumlah FROM aset, koleksi_aset_memiliki_aset WHERE id=id_aset;"
-            cursor.execute(list_aset)
-            data = cursor.fetchall()
-            return render(request, 'list_koleksi_aset.html',{"data":data})
+    list_aset = "SELECT email, nama, minimum_level, harga_beli, jumlah FROM aset, koleksi_aset_memiliki_aset, pengguna WHERE email=id_koleksi_aset AND id=id_aset AND id_aset LIKE '%DK%';"
+    cursor.execute(list_aset)
+    data = cursor.fetchall()
+    return render(request, 'list_koleksi_dekorasi.html', {'data': data, 'role': role})
         
 
 def list_dekorasi(request):
@@ -116,6 +184,22 @@ def list_bibit_tanaman(request):
     data = cursor.fetchall()
     return render(request, 'list_bibit_tanaman.html', {'data': data,'role': role})
 
+def list_koleksi_bibit(request):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
+    cursor.execute("SET search_path TO hidayf14") 
+
+    list_aset = "SELECT email, nama, minimum_level, harga_beli, jumlah FROM aset, koleksi_aset_memiliki_aset, pengguna WHERE email=id_koleksi_aset AND id=id_aset AND id_aset LIKE '%BT%';"
+    cursor.execute(list_aset)
+    data = cursor.fetchall()
+    return render(request, 'list_koleksi_bibit.html', {'data': data, 'role': role})
+
 def list_kandang(request):
     cursor = connection.cursor()
     
@@ -132,6 +216,22 @@ def list_kandang(request):
     data = cursor.fetchall()
     return render(request, 'list_kandang.html', {'data': data,'role': role})
 
+def list_koleksi_kandang(request):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
+    cursor.execute("SET search_path TO hidayf14") 
+
+    list_aset = "SELECT email, nama, minimum_level, harga_beli, jumlah FROM aset, koleksi_aset_memiliki_aset, pengguna WHERE email=id_koleksi_aset AND id=id_aset AND id_aset LIKE '%KD%';"
+    cursor.execute(list_aset)
+    data = cursor.fetchall()
+    return render(request, 'list_koleksi_kandang.html', {'data': data, 'role': role})
+
 def list_hewan(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
@@ -146,6 +246,22 @@ def list_hewan(request):
     cursor.execute(query)
     data = cursor.fetchall()
     return render(request, 'list_hewan.html', {'data': data,'role': role})
+
+def list_koleksi_hewan(request):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
+    cursor.execute("SET search_path TO hidayf14") 
+
+    list_aset = "SELECT email, nama, minimum_level, harga_beli, jumlah FROM aset, koleksi_aset_memiliki_aset, pengguna WHERE email=id_koleksi_aset AND id=id_aset AND id_aset LIKE '%HW%';"
+    cursor.execute(list_aset)
+    data = cursor.fetchall()
+    return render(request, 'list_koleksi_kandang.html', {'data': data, 'role': role})
 
 def list_alat_produksi(request):
     cursor = connection.cursor()
@@ -162,6 +278,22 @@ def list_alat_produksi(request):
     data = cursor.fetchall()
     return render(request, 'list_alat_produksi.html', {'data': data,'role': role})
 
+def list_koleksi_alat_produksi(request):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            role = "admin"
+        else:
+            role = None
+    cursor.execute("SET search_path TO hidayf14") 
+
+    list_aset = "SELECT email, nama, minimum_level, harga_beli, jumlah FROM aset, koleksi_aset_memiliki_aset, pengguna WHERE email=id_koleksi_aset AND id=id_aset AND id_aset LIKE '%AP%';"
+    cursor.execute(list_aset)
+    data = cursor.fetchall()
+    return render(request, 'list_koleksi_alat.html', {'data': data, 'role': role})
+
 def list_petak_sawah(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
@@ -177,63 +309,92 @@ def list_petak_sawah(request):
     data = cursor.fetchall()
     return render(request, 'list_petak_sawah.html', {'data': data,'role': role})
 
-def update_dekorasi(request):
+def list_koleksi_petak_sawah(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
-    if request.session.has_key('account'):
-        if request.session['role'] == "admin":
-            role = "admin"
-        else:
-            role = None
-    return render(request, 'update_dekorasi.html', {'role': role})
 
-def update_bibit(request):
-    cursor = connection.cursor()
-    cursor.execute("SET search_path TO public")
     if request.session.has_key('account'):
         if request.session['role'] == "admin":
             role = "admin"
         else:
             role = None
-    return render(request, 'update_bibit_tanaman.html', {'role': role})
+    cursor.execute("SET search_path TO hidayf14") 
 
-def update_kandang(request):
-    cursor = connection.cursor()
-    cursor.execute("SET search_path TO public")
-    if request.session.has_key('account'):
-        if request.session['role'] == "admin":
-            role = "admin"
-        else:
-            role = None
-    return render(request, 'update_kadandang.html', {'role': role})
+    list_aset = "SELECT email, nama, minimum_level, harga_beli, jumlah FROM aset, koleksi_aset_memiliki_aset, pengguna WHERE email=id_koleksi_aset AND id=id_aset AND id_aset LIKE '%PS%';"
+    cursor.execute(list_aset)
+    data = cursor.fetchall()
+    return render(request, 'list_koleksi_alat.html', {'data': data, 'role': role})
 
-def update_hewan(request):
-    cursor = connection.cursor()
-    cursor.execute("SET search_path TO public")
-    if request.session.has_key('account'):
-        if request.session['role'] == "admin":
-            role = "admin"
-        else:
-            role = None
-    return render(request, 'update_hewan.html', {'role': role})
 
-def update_alat(request):
+def update_dekorasi(request, nama):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
     if request.session.has_key('account'):
         if request.session['role'] == "admin":
-            role = "admin"
+            if request.method == "POST":
+                return redirect("aset:list-dekorasi")
+            else:
+                return render(request, 'update_dekorasi.html', {'nama': nama, 'role': request.session['role']})
         else:
-            role = None
-    return render(request, 'update_alat_produksi.html', {'role': role})
+            return redirect("aset:list-dekorasi")
 
-def update_petak_sawah(request):
+def update_bibit(request, nama):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
     if request.session.has_key('account'):
         if request.session['role'] == "admin":
-            role = "admin"
+            if request.method == "POST":
+                return redirect("aset:list-bibit-tanaman")
+            else:
+                return render(request, 'update_bibit_tanaman.html', {'nama': nama, 'role': request.session['role']})
         else:
-            role = None
-    return render(request, 'update_petak_sawah.html', {'role': role})
+            return redirect("aset:list-bibit-tanaman")
+
+def update_kandang(request, nama):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            if request.method == "POST":
+                return redirect("aset:list-kandang")
+            else:
+                return render(request, 'update_kadandang.html', {'nama': nama, 'role': request.session['role']})
+        else:
+            return redirect("aset:list-kandang")
+
+def update_hewan(request, nama):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            if request.method == "POST":
+                return redirect("aset:list-hewan")
+            else:
+                return render(request, 'update_hewan.html', {'nama': nama, 'role': request.session['role']})
+        else:
+            return redirect("aset:list-hewan")
+
+def update_alat(request, nama):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            if request.method == "POST":
+                return redirect("aset:list-alat-produksi")
+            else:
+                return render(request, 'update_alat_produksi.html', {'nama': nama, 'role': request.session['role']})
+        else:
+            return redirect("aset:list-alat-produksi")
+
+def update_petak_sawah(request, nama):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    if request.session.has_key('account'):
+        if request.session['role'] == "admin":
+            if request.method == "POST":
+                return redirect("aset:list-petak-sawah")
+            else:
+                return render(request, 'update_petak_sawah.html', {'nama': nama, 'role': request.session['role']})
+        else:
+            return redirect("aset:list-petak-sawah")
     
