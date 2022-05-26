204937c0d229ccd13df23c1f2e598bb15142e430
Mohammad Bryan Mahdavikhia <mohammad.bryan@ui.ac.id>
2022-05-26 15:52:52 +0700

    Implements CR transaksi pembelian koin

--- paket_koin/views.py
+++ paket_koin/views.py
@@ -2,6 +2,7 @@ from hashlib import new
 import re
 from django.db import connection
 from django.shortcuts import redirect, render
+from collections import namedtuple
 
 # Create your views here.
 def create_paket(request):
@@ -39,7 +40,7 @@ def list_paket(request):
     cursor.execute("SET search_path TO hidayf14")
     cursor.execute("SELECT * FROM paket_koin")
     data = cursor.fetchall()
-    cursor.execute("SELECT distinct paket_koin, total_biaya FROM transaksi_pembelian_koin")
+    cursor.execute("SELECT distinct tpk.paket_koin, pk.harga FROM transaksi_pembelian_koin tpk, paket_koin pk WHERE tpk.paket_koin = pk.jumlah_koin")
     hapus = cursor.fetchall()
 
     if request.method == "POST":
@@ -106,9 +107,15 @@ def beli_paket_koin(request, value, harga):
             if request.method == "POST":
                 jumlah = request.POST["jumlah"]
                 cara = request.POST["cara"]
+                total_biaya = int(jumlah)*int(harga)
                 
-                # cursor.execute("SET search_path TO hidayf14")
-                # cursor.execute("INSERT INTO paket_koin VALUES (%s, %s)", [value, price])
+                cursor.execute("SET search_path TO hidayf14")
+                cursor.execute("select (now() + interval '7 hours')::timestamp")
+                ts = tuple_fetch(cursor)
+                time_str = str(ts[0][0])
+                    
+                
+                cursor.execute("INSERT INTO transaksi_pembelian_koin VALUES ('"+request.session['account'][0]+"', '"+time_str + "'::timestamp, '"+ str(jumlah)+"', '"+str(cara)+"', '"+str(value)+"', '"+ str(total_biaya)+"')")
                 return redirect("paket_koin:list_transaksi")
             else:
                 return render(request, "beli_paket_koin.html", {'value': value, 'role': role, 'harga': harga})
@@ -118,4 +125,7 @@ def beli_paket_koin(request, value, harga):
         return redirect("home:login")
 
 
-
+def tuple_fetch(cursor):
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
