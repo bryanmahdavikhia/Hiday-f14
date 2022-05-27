b2bcb05cb9c02b18b182e5dfd49f8ac27aaeb14f
Raissa Tito Safaraz <raissa.tito@ui.ac.id>
2022-05-27 09:25:55 +0700

    Finished produk implemetation

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.db import connection
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
+from django.contrib import messages
 
 
 def home(request):
@@ -41,10 +42,10 @@ def login(request):
                     request.session['account'] = account
                     request.session['role'] = role
                     return HttpResponseRedirect("/main")
-                
+            
+            messages.add_message(request, messages.INFO, 'Email atau password anda salah')
             cursor.execute("SET search_path TO public")
             return redirect("home:login")
-        
         else:
             cursor.execute("SET search_path TO public")
             return render(request, "login.html", {})
@@ -55,11 +56,14 @@ def main(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
     if request.session.has_key('account'):
+        email = request.session['account'][0]
         if request.session['role'] == "admin":
-            role = "admin"
+            farm_details = None
         else:
-            role = None
-        return render(request, 'main.html', {'role': role})
+            cursor.execute("SET search_path TO hidayf14")
+            cursor.execute("SELECT * FROM PENGGUNA WHERE email = %s", [email])
+            farm_details = cursor.fetchall()
+        return render(request, 'main.html', {'email':email, 'farm_details':farm_details})
     else:
         return redirect("home:login")
 
@@ -71,9 +75,44 @@ def logout(request):
         pass
     return HttpResponseRedirect("/")
 
+def register_option(request):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    if not request.session.has_key('account'):
+        return render(request, 'role.html', {})
+    else: 
+        return redirect("home:main")
 
+def register(request, id):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
 
-        
-        
-
-
+    if not request.session.has_key('account'):
+        cursor.execute("SET search_path TO hidayf14")
+        if request.method == "POST":
+            email = request.POST["email"]
+            password = request.POST["password"]
+            farm_name = request.POST["farm_name"]
+            cursor.execute("SELECT email FROM akun WHERE email = %s", [email])
+            target = cursor.fetchone()
+            if target == None:
+                cursor.execute("INSERT INTO akun VALUES (%s)", [email])
+                if id == "admin":
+                    cursor.execute("INSERT INTO admin VALUES (%s, %s)", [email, password])
+                else:
+                    cursor.execute("INSERT INTO pengguna VALUES (%s, %s, %s)", [email, password, farm_name])
+                cursor.execute("SET search_path TO public")
+                request.session['account'] = [email]
+                request.session['role'] = id
+                return HttpResponseRedirect("/main")
+            else:
+                messages.add_message(request, messages.INFO, 'Email sudah terambil. silahkan gunakan email lain')
+                return redirect("home:register")
+        else:
+            if id == "admin":
+                user = None
+            else:
+                user = "exist"
+            return render(request, 'register.html', {'user':user})
+    else:
+        return redirect("home:main")
--- produk/urls.py
+++ produk/urls.py
@@ -8,11 +8,13 @@ app_name = 'product'
 urlpatterns = [
     path('create-product', views.create_product, name='create_product'),
     path('', views.list_product, name='list_product'),
-    path('update-product', views.update_product, name='update_product'),
+    path('update-product/<str:id>', views.update_product, name='update_product'),
+    path('delete-product/<str:id>', views.delete_product, name='delete_product'),
     path('create-production', views.create_production, name='create_product'),
     path('list-production', views.list_production, name='list_production'),
     path('detail-production/<int:id>', views.detail_production, name='detail_production'),
-    path('update-production', views.update_production, name='update_production'),
+    path('update-production/<int:id>', views.update_production, name='update_production'),
+    path('delete-production/<int:id>', views.delete_production, name='delete_production'),
     path('create-history', views.create_product_history, name='create_product_history'),
     path('list-history', views.list_product_history, name='list_history')
 ]
